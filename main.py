b09b455002d5743b70b11ac37684a89d5dda4704
baranigit62 <igitbaran83@gmail.com>
2025-03-06 12:30:08 +0100

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-class myclass:
+class person:
   def __init__(self, name, age):
     self.name = name
     self.age = age
-p1 = myclass("baran", 19)
+p1 = person("baran", 19)
 print(p1.name)
 print(p1.age)
