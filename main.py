e55b1e11eb58ac2f4c54ff3d55fcaa193a8d8ca5
baranigit62 <igitbaran83@gmail.com>
2025-03-20 12:30:15 +0100

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-class person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-p1 = person("baran", 19)
-print(p1.name)
-print(p1.age)
+
+animals = [
+    {
+        'name': 'Penguin',
+        'group': 'Birds',
+        'number_of_legs': 2,
+        'skills': ['swimming', 'sliding on belly']
+    },
+    {
+        'name': 'Kangaroo',
+        'group': 'Mammals',
+        'number_of_legs': 4,
+        'skills': ['jumping', 'running']
+    },
+    {
+        'name': 'Crocodile',
+        'group': 'Reptiles',
+        'number_of_legs': 4,
+        'skills': ['swimming', 'hunting']
+    },
+    {
+        'name': 'Bat',
+        'group': 'Mammals',
+        'number_of_legs': 2,
+        'skills': ['flying', 'echolocation']
+    },
+    {
+        'name': 'Octopus',
+        'group': 'Mollusks',
+        'number_of_legs': 8,
+        'skills': ['swimming', 'camouflage', 'problem-solving']
+    }
+]
+
+for animal in animals:
+    print(f"{animal['name']} belongs to the group of {animal['group']} and has {animal['number_of_legs']} legs.")
+    print(f"Skills: {', '.join(animal['skills'])}")
