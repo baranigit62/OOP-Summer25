592cf0008d76b3eb0a9c84cfb5b242a62f930de1
baranigit62 <igitbaran83@gmail.com>
2025-04-03 10:40:40 +0000

    main.py

---
+++ w3_school/python_data_types.py
@@ -0,0 +1,23 @@
+x = 5
+print(type(x))  # Output: <class 'int'>
+
+y = 3.14
+print(type(y))  # Output: <class 'float'>
+
+name = "Alice"
+print(type(name))  # Output: <class 'str'>
+
+is_active = True
+print(type(is_active))  # Output: <class 'bool'>
+
+numbers = [1, 2, 3, 4]
+print(type(numbers))  # Output: <class 'list'>
+
+coordinates = (10, 20)
+print(type(coordinates))  # Output: <class 'tuple'>
+
+person = {"name": "Bob", "age": 30}
+print(type(person))  # Output: <class 'dict'>
+
+fruits = {"apple", "banana", "cherry"}
+print(type(fruits))  # Output: <class 'set'>
